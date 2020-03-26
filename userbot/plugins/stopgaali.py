db44ef959d3e2839f5774e68eedf1d62e6c01026
Zylern <47510180+Zylern@users.noreply.github.com>
2020-03-26 12:46:51 +0530

    Create stopgaali.py

---
+++ userbot/plugins/stopgaali.py
@@ -0,0 +1,52 @@
+from telethon import events
+import random, re
+from userbot.utils import admin_cmd
+import asyncio 
+
+
+
+@borg.on(admin_cmd("stop_gaali ?(.*)"))
+async def _(event):
+     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
+        await event.edit("Gaali")
+        await asyncio.sleep(0.3)
+        await event.edit("Mat")
+        await asyncio.sleep(0.2)
+        await event.edit("Do")
+        await asyncio.sleep(0.5)
+        await event.edit("Gaali")
+        await asyncio.sleep(0.2)
+        await event.edit("Mat")
+        await asyncio.sleep(0.3)
+        await event.edit("Lo")
+        await asyncio.sleep(0.3)
+        await event.edit("Itne")
+        await asyncio.sleep(0.3)
+        await event.edit("Ghade")
+        await asyncio.sleep(0.3)
+        await event.edit("Khod")
+        await asyncio.sleep(0.3)
+        await event.edit("Dunga")
+        await asyncio.sleep(0.3)
+        await event.edit("Agle")
+        await asyncio.sleep(0.3)
+        await event.edit("Janam")
+        await asyncio.sleep(0.3)
+        await event.edit("Me")
+        await asyncio.sleep(0.3)
+        await event.edit("Tera")
+        await asyncio.sleep(0.3)
+        await event.edit("Baap")
+        await asyncio.sleep(0.3)
+        await event.edit("Bankar")
+        await asyncio.sleep(0.3)
+        await event.edit("Teri")
+        await asyncio.sleep(0.3)
+        await event.edit("Maa")
+        await asyncio.sleep(0.3)
+        await event.edit("Chod")
+        await asyncio.sleep(0.3)
+        await event.edit("Dunga")
+        await asyncio.sleep(0.3)
+        
+        await event.edit("**Gaali Mat Do Gaali Mat Lo Itne GhandeKhod Dunga Agle Janam Me Tera Baap Bankar Teri Maa Chod Dunga.**")
