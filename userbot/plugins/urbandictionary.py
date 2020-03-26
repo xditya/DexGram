b02f2f108dbb8d6eab32157e00d2f6aba550d12e
Zylern <47510180+Zylern@users.noreply.github.com>
2020-03-26 18:26:30 +0530

    Update urbandictionary.py

--- userbot/plugins/urbandictionary.py
+++ userbot/plugins/urbandictionary.py
@@ -2,13 +2,13 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 """Urban Dictionary
-Syntax: .ud Query"""
+Syntax: .dictionary Query"""
 from telethon import events
 import asyncurban
 from userbot.utils import admin_cmd
 
 
-@borg.on(admin_cmd("ud (.*)"))
+@borg.on(admin_cmd("dictionary (.*)"))
 async def _(event):
     if event.fwd_from:
         return
