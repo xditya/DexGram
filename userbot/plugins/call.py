8d22e6e81d4e64d6b3e695a15df82f3bf01044b0
Zylern <47510180+Zylern@users.noreply.github.com>
2020-03-26 10:52:30 +0530

    Update call.py

--- userbot/plugins/call.py
+++ userbot/plugins/call.py
@@ -1,6 +1,8 @@
 """Emoji
 
 Available Commands:
+	
+.call
 
 .emoji shrug
 
@@ -40,21 +42,21 @@ async def _(event):
         
             "`Connecting To Telegram Headquarters...`",
             "`Call Connected.`",
-            "`Telegram: Hello This is Telegram HQ. Who is this?`",
-            "`Me: Yo this is` @Hack12R ,`Please Connect me to my lil bro,Pavel Durov`",
+            "`Telegram: Hello This is Telegram Head Quarter. Who is this?`",
+            "`Me: Yo this is` Zylern ,`Please Connect me to my lil bro,Dorber`",
             "`User Authorised.`",
-            "`Calling Pavel Durov`  `At +916969696969`",
+            "`Calling Pavel Dorber`  `At +916969696969`",
             "`Private  Call Connected...`",
             "`Me: Hello Sir, Please Ban This Telegram Account.`",    
-            "`Pavel: May I Know Who Is This?`",
-            "`Me: Yo Brah, I Am` @Hack12R ",
-            "`Pavel: OMG!!! Long time no see, Wassup Brother...\nI'll Make Sure That Guy Account Will Get Blocked Within 24Hrs.`",
+            "`Dorber: May I Know Who Is This?`",
+            "`Me: Yo Brah, I Am` Zylern. ",
+            "`Dorber: OMG!!! Long time no see, Wassup Brother...\nI'll Make Sure That Guy Account Will Get Blocked Within 24Hrs.`",
             "`Me: Thanks, See You Later Brah.`",
-            "`Pavel: Please Don't Thank Brah, Telegram Is Our's. Just Gimme A Call When You Become Free.`",
+            "`Dorber: Please Don't Thank Bro, Telegram Is Our's. Just Give me A Call When You Become Free.`",
             "`Me: Is There Any Issue/Emergency???`",
-            "`Pavel: Yes Sur, There Is A Bug In Telegram v69.6.9.\nI Am Not Able To Fix It. If Possible, Please Help Fix The Bug.`",
+            "`Dorber: Yes Sur, There Is A Bug In Telegram v69.6.9.\nI Am Not Able To Fix It. If Possible, Please Help Fix The Bug.`",
             "`Me: Send Me The App On My Telegram Account, I Will Fix The Bug & Send You.`",
-            "`Pavel: Sure Sur \nTC Bye Bye :)`",
+            "`Dorber: Sure Guruji \nTC Bye Bye :)`",
             "`Private Call Disconnected.`"
         ]
 
