9982711a4bbec4e94545f58726ba1c9554de0cd6
Zylern <47510180+Zylern@users.noreply.github.com>
2020-03-31 10:30:24 +0530

    Update pmpermit_menu.py

--- userbot/plugins/pmpermit_menu.py
+++ userbot/plugins/pmpermit_menu.py
@@ -41,7 +41,7 @@ async def _(event):
                "`3`. To send nudes.\n"
                "`4`. To enquire something\n"
                "`5`. To request something\n")
-         ONE = ("__Okay. Your request has been registered. Do not spam my master's inbox, NHI TOH PELE JAAOGE.You can expect a reply within 24 light years. He is a busy man, unlike you probably.__\n\n"
+         ONE = ("__Okay. Your request has been registered. Do not spam my master's inbox. You can expect a reply within 24 decades. He is a busy man, unlike you probably.__\n\n"
                 "**⚠️ You will be blocked and reported if you spam nibba. ⚠️**\n\n"
                 "__Use__ `/start` __to go back to the main menu.__")
          TWO = (" `███████▄▄███████████▄  \n▓▓▓▓▓▓█░░░░░░░░░░░░░░█\n▓▓▓▓▓▓█░░░░░░░░░░░░░░█\n▓▓▓▓▓▓█░░░░░░░░░░░░░░█\n▓▓▓▓▓▓█░░░░░░░░░░░░░░█\n▓▓▓▓▓▓█░░░░░░░░░░░░░░█\n▓▓▓▓▓▓███░░░░░░░░░░░░█\n██████▀▀▀█░░░░██████▀  \n░░░░░░░░░█░░░░█  \n░░░░░░░░░░█░░░█  \n░░░░░░░░░░░█░░█  \n░░░░░░░░░░░█░░█  \n░░░░░░░░░░░░▀▀ `\n\n**So uncool, this is not your home. Go bother someone else. You have been blocked and reported until further notice.**")
