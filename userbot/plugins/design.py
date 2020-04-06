8f71ecc9e5ef216e53a40f5dfeccccb131757c4c
Zylern <47510180+Zylern@users.noreply.github.com>
2020-04-06 20:31:58 +0530

    Update design.py

--- userbot/plugins/design.py
+++ userbot/plugins/design.py
@@ -1,4 +1,4 @@
-"""COMMAND : .fuck , .work , .push , .aag , .climb"""
+"""COMMAND : .fuck , .work , .push , .climb"""
 import asyncio
 from telethon import events
 from telethon.tl.types import ChannelParticipantsAdmins
@@ -38,21 +38,6 @@ async def _(event):
         await event.reply(mentions)
     await event.delete()
 
-@borg.on(admin_cmd("aag"))
-async def _(event):
-    if event.fwd_from:
-        return
-    mentions = "`😲💨  🔥\n/|\     🔥🔥\n/ \   🔥🔥🔥`"
-    chat = await event.get_input_chat()
-    async for x in borg.iter_participants(chat, filter=ChannelParticipantsAdmins):
-        mentions += f""
-    reply_message = None
-    if event.reply_to_msg_id:
-        reply_message = await event.get_reply_message()
-        await reply_message.reply(mentions)
-    else:
-        await event.reply(mentions)
-    await event.delete()
 
 @borg.on(admin_cmd("push"))
 async def _(event):
