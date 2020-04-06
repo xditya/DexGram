1bdaf028d0a1bd05f9f01a3a799fa734b77e1ac3
Zylern <47510180+Zylern@users.noreply.github.com>
2020-04-06 21:49:25 +0530

    Add files via upload

---
+++ userbot/plugins/5_6079862026685907207.py
@@ -0,0 +1,51 @@
+"""COMMAND : .aag"""
+
+from telethon import events
+
+import asyncio
+
+
+
+
+
+@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    animation_interval = 40
+    
+
+    animation_ttl = range(0, 103)
+
+    input_str = event.pattern_match.group(1)
+
+    if input_str == "aag":
+
+        await event.edit(input_str)
+
+        animation_chars = [
+
+            "Aag Lagi",
+            "🔥",    
+            "🌬💨⁠          🔥",
+            "🌬 💨         🔥",
+            "🌬   💨       🔥",
+            "🌬    💨      🔥",
+            "🌬     💨     🔥",
+            "🌬      💨    🔥",
+            "🌬       💨   🔥",    
+            "🌬         💨 🔥",
+            "🌬          💨🔥",
+            "🌬             💨",
+            "**Aag Bhuj Gyi** 😁",
+
+        ]
+
+        for i in animation_ttl:
+
+
+            await event.edit(animation_chars[i % 103])
