cb8ebcf32ec94047aa496ca7fae987a51df5275a
Zylern <47510180+Zylern@users.noreply.github.com>
2020-04-06 20:45:24 +0530

    Update aag.py

--- userbot/plugins/aag.py
+++ userbot/plugins/aag.py
@@ -5,7 +5,7 @@ from telethon import events
 from uniborg.util import admin_cmd
 import asyncio
 
-@borg.on(admin_cmd(pattern="aag ?(.*)"))
+@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
 async def _(event):
 
     if event.fwd_from:
