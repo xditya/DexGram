cd0a0ba525e6550383d19d6363d4f123d9a324ab
Zylern <47510180+Zylern@users.noreply.github.com>
2020-04-06 21:30:50 +0530

    Update aag.py

--- userbot/plugins/aag.py
+++ userbot/plugins/aag.py
@@ -14,7 +14,7 @@ async def _(event):
     animation_interval = 40
     
 
-    animation_ttl = range(0, 40)
+    animation_ttl = range(0, 39)
 
     input_str = event.pattern_match.group(1)
 
@@ -69,4 +69,4 @@ async def _(event):
         for i in animation_ttl:
 
 
-            await event.edit(animation_chars[i % 40)
+            await event.edit(animation_chars[i % 39)
