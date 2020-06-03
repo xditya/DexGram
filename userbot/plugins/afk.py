fcfa512b4fc0963f737c86fac768369ee851f914
Zylern <47510180+Zylern@users.noreply.github.com>
2020-06-03 19:22:43 +0530

    Update afk.py

--- userbot/plugins/afk.py
+++ userbot/plugins/afk.py
@@ -145,8 +145,8 @@ async def on_afk(event):
             else:
                 afk_since = f"`{int(seconds)}s` **ago**"
         msg = None
-        message_to_reply = f"__My Master Has Been Gone For__ `{total_afk_time}`\nWhere He Is: **Tujhe Kyu Batau.** " + \
-            f"\n\n__I promise I'll back in a few light years__\n**REASON**: {reason}" \
+        message_to_reply = f"__My Master Has Been Gone For__ `{total_afk_time}" + \
+            f"\n\n**REASON**: {reason}" \
             if reason \
             else f"**Heya!**\n__I am currently unavailable. Since when, you ask? For {total_afk_time} I guess.__\n\nWhen will I be back? ~~Soon~~ __Whenever I feel like it.__"
         msg = await event.reply(message_to_reply)
